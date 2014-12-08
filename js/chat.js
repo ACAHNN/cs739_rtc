9d4dd93352f426983558cd5af21c345ab98bac04
Peng Liu <pengliu.mail@gmail.com>
2014-12-07 21:49:56 -0600

    We can chat now!

--- js/chat.js
+++ js/chat.js
@@ -3,6 +3,8 @@ msgformPost = function () {
   var input = document.getElementById("msg_form");
   var inputData = encodeURIComponent(input.value);
   sendMessage("/send_message", "to=" + receiverName + "&msg=" + inputData);
+  messages.push({"msg": input.value});
+  updateMessageWindow();
   input.value = '';
   document.getElementById("msg_form").focus();
 }
@@ -32,7 +34,37 @@ onOpened = function() {
 onMessage = function(m) {
   newMessage = JSON.parse(m.data);
   console.log(newMessage);
+  messages.push(newMessage);
+  updateMessageWindow();
+}
+
+updateMessageWindow = function() {
+  var htmlString = "<ul>";
+
+  console.log(messages);
+
+  for (i = 0; i < messages.length; i++) {
+    if (messages[i].from) {
+      htmlString += "<li><div class=\"bubble\"><a class=\"user-name\" href=\"\">";
+      htmlString += messages[i].from;      
+    }
+    else {
+      htmlString += "<li class=\"current-user\"><div class=\"bubble\"><a class=\"user-name\" href=\"\">";
+      htmlString += "Me";
+    }
+
+    htmlString += "</a><p class=\"message\">"
+    htmlString += messages[i].msg;
+    htmlString += "</p>";
+    htmlString += "</div></li>"
+  }
+
+  htmlString += "</ul>";
+
+  $("#message_window").html(htmlString);
   
+  var objDiv = document.getElementById("message_window");
+  objDiv.scrollTop = objDiv.scrollHeight;
 }
 
 openChannel = function() {
@@ -54,12 +86,18 @@ initialize = function() {
 }
 
 var receiverName;
+var messages = [];
 setTimeout(initialize, 100);
+
 $(document).ready(function() {
   $('.friend').on("click", function() {
     var user_name =  $(this).attr("id");
-    receiverName = user_name;
-    $("#chat_title").html("<i class=\"icon-comments\"></i>Chat with " + user_name + "<i class=\"icon-cog pull-right\"></i><i class=\"icon-smile pull-right\"></i>");
-    $("#message_input").html("<form action=\"javascript:msgformPost();\"><input class=\"form-control\" placeholder=\"Input Message...\" type=\"text\" id=\"msg_form\"><input type=\"submit\" value=\"Send\" id=\"send_msg_btn\"></form>")
+    if (receiverName != user_name) {
+      receiverName = user_name;
+      $("#chat_title").html("<i class=\"icon-comments\"></i>Chat with " + user_name + "<i class=\"icon-cog pull-right\"></i><i class=\"icon-smile pull-right\"></i>");
+      $("#message_input").html("<form action=\"javascript:msgformPost();\"><input class=\"form-control\" placeholder=\"Input Message...\" type=\"text\" id=\"msg_form\"><input type=\"submit\" value=\"Send\" id=\"send_msg_btn\"></form>");
+      messages = [];
+    }
+    document.getElementById("msg_form").focus();
   });
 });
